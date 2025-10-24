7741fbc090c17e98ead496232fbbfed58b04fa3d
Baran Buzluk <baranbuzluk@gmail.com>
2025-10-24 22:55:24 +0300

    chore: init project

---
+++ src/test/java/com/github/bbuzluk/surl/SurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bbuzluk.surl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
