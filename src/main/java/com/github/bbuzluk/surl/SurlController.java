9a4773d83e5690cd5da4ad76731344deb9184bb7
Baran Buzluk <baranbuzluk@gmail.com>
2025-11-03 21:13:19 +0300

    build(version): bump version to 0.0.0

--- src/main/java/com/github/bbuzluk/surl/SurlController.java
+++ src/main/java/com/github/bbuzluk/surl/SurlController.java
@@ -1,5 +1,7 @@
 package com.github.bbuzluk.surl;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +12,8 @@ public class SurlController {
   @GetMapping("/version")
   public Map<Object, Object> getVersion() {
     Map<Object, Object> versionInfo = new HashMap<>();
-    versionInfo.put("version", null);
-    versionInfo.put("buildDate", null);
+    versionInfo.put("version", "0.0.0");
+    versionInfo.put("buildDate", LocalDate.of(2025, Month.NOVEMBER, 2));
     return versionInfo;
   }
 }
